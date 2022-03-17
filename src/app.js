c8e3da6d6b09ef0fdc5ee97463e51e41e22d9d61
Marcus-Schebek <43122453+Marcus-Schebek@users.noreply.github.com>
2022-03-16 21:35:26 -0300

    Added Dark Mode

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 let addBtn = document.querySelector("#push");
 let inputTask = document.querySelector("#newTask input");
 let tasks = document.querySelector("#tasks");
-let task = document.querySelector('.task')
+let task = document.querySelector(".task");
 let taksOnDiv = 0;
 
 addBtn.onclick = () => {
@@ -9,44 +9,59 @@ addBtn.onclick = () => {
     alert("Please input a task");
   } else {
     tasks.innerHTML += `
-    <div class="task">
-        <span id="taskname"> 
-        ${inputTask.value}
-        </span>
-        <button class="delete">
-            <i class="far fa-trash-alt"></i>
-        </button>
-    </div>`;
+      <div class="task">
+          <span id="taskname"> 
+          ${inputTask.value}
+          </span>
+          <button class="delete">
+              <i class="far fa-trash-alt"></i>
+          </button>
+      </div>`;
 
-    let current_tasks = document.querySelectorAll(".delete");
-    for(let i = 0; i<current_tasks.length; i++){
-        current_tasks[i].onclick = function() {
-            this.parentNode.remove();
-            taksOnDiv--;
-            hiddenElement(tasks)
-        }
+    current_tasks = document.querySelectorAll(".delete");
+    for (let i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+        taksOnDiv--;
+        hiddenElement(tasks);
+        return current_tasks;
+      };
     }
     let tasksComplete = document.querySelectorAll(".task");
-    for(let i=0; i<tasksComplete.length; i++){
-        tasksComplete[i].onclick = function(){
-            this.classList.toggle('completed');
-        }
+    for (let i = 0; i < tasksComplete.length; i++) {
+      tasksComplete[i].onclick = function () {
+        this.classList.toggle("completed");
+      };
     }
     inputTask.value = "";
     taksOnDiv++;
-    hiddenElement(tasks)
+    hiddenElement(tasks);
+    console.log(addBtn.backgroundColor);
   }
 };
 
-function hiddenElement (element){
-  if(taksOnDiv !== 0){
-    element.style.display = 'block'
-  } else{
-    element.style.display = 'none';
+function hiddenElement(element) {
+  if (taksOnDiv !== 0) {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
   }
 }
-hiddenElement(tasks)
 
-addBtn
+hiddenElement(tasks);
+
+const chk = document.querySelector(".checkbox");
+const darkThemeChanger = document.querySelector(".label");
+
+chk.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
+  addBtn.classList.toggle("dark");
+  darkThemeChanger.classList.toggle("dark");
+  if (chk.checked) {
+    current_tasks.forEach((x) => x.classList.add("dark"));
+  } else {
+    current_tasks.forEach((x) => x.classList.remove("dark"));
+  }
+});
 
 
